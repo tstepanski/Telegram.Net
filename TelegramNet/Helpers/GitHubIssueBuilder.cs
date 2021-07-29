d83d9c42c7886e3704a181387e678e3ff52edb29
Denis <69825463+DenchickPenchick@users.noreply.github.com>
2021-07-29 16:52:58 +0300

    🦾 Issue system update

---
+++ TelegramNet/Helpers/GitHubIssueBuilder.cs
@@ -0,0 +1,8 @@
+namespace TelegramNet.Helpers
+{
+    internal static class GitHubIssueBuilder
+    {
+        public static string Message(string message, string head, string body) =>
+            $"{message}\nReport it on GitHub: https://github.com/DenVot/TelegramNet/issues/new?title={head.Replace("\n", "%0D%0A")}&body={body.Replace("\n", "%0D%0A")}";
+    }
+}
