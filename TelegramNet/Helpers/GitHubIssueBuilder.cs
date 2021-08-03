ed69bfde3f2c8eea6147252287eadc80e3a457f3
Denis <69825463+DenchickPenchick@users.noreply.github.com>
2021-08-03 14:38:32 +0300

    🐭 GitHub support added

--- TelegramNet/Helpers/GitHubIssueBuilder.cs
+++ TelegramNet/Helpers/GitHubIssueBuilder.cs
@@ -2,7 +2,10 @@ namespace TelegramNet.Helpers
 {
     internal static class GitHubIssueBuilder
     {
-        public static string Message(string message, string head, string body) =>
-            $"{message}\nReport it on GitHub: https://github.com/DenVot/TelegramNet/issues/new?title={head.Replace("\n", "%0D%0A")}&body={body.Replace("\n", "%0D%0A")}";
+        public static string Message(string message, string head, string body)
+        {
+            return
+                $"{message}\nReport it on GitHub: https://github.com/DenVot/TelegramNet/issues/new?title={head.Replace("\n", "%0D%0A")}&body={body.Replace("\n", "%0D%0A")}";
+        }
     }
 }
