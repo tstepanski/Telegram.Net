b4c8b136186b4c2284656d38b01bf85e73e9471f
Denis <69825463+DenchickPenchick@users.noreply.github.com>
2021-08-08 20:09:37 +0300

    👌 MessageCaption bug fixes

--- TelegramNet/Types/MessageEntity.cs
+++ TelegramNet/Types/MessageEntity.cs
@@ -1,17 +1,25 @@
+using System.Text.Json.Serialization;
+
 namespace TelegramNet.Types
 {
     internal class MessageEntity
     {
+        [JsonPropertyName("type")]
         public string Type { get; set; }
 
+        [JsonPropertyName("offset")]
         public int Offset { get; set; }
 
+        [JsonPropertyName("length")]
         public int Length { get; set; }
 
+        [JsonPropertyName("url")]
         public string Url { get; set; }
 
+        [JsonPropertyName("user")]
         public User User { get; set; }
 
+        [JsonPropertyName("language")]
         public string Language { get; set; }
     }
 }
