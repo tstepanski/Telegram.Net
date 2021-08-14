a9f3e9ac670ee144d22495edee48ce0d6d5f6120
Timothy M. Stepanski <timothy@timothystepanski.com>
2021-08-14 10:45:43 -0400

    moved to async processor

--- TelegramNet/Services/ReceivingUpdates/UpdatingWorker.cs
+++ TelegramNet/Services/ReceivingUpdates/UpdatingWorker.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Linq;
 using System.Net.Http;
-using System.Threading;
+using System.Threading.Tasks;
 using TelegramNet.Helpers;
 using TelegramNet.Logging;
 
@@ -9,6 +9,9 @@ namespace TelegramNet.Services.ReceivingUpdates
 {
     internal class UpdatingWorker
     {
+	    private static readonly TimeSpan OneSecond = TimeSpan.FromSeconds(1);
+	    private readonly TelegramApiClient _api;
+	    private bool _stopped;
         private int _lastId;
 
         public UpdatingWorker(BaseTelegramClient client)
@@ -16,54 +19,50 @@ namespace TelegramNet.Services.ReceivingUpdates
             _api = client.TelegramApi;
         }
 
-        private bool _stop;
-        private Thread _thr;
-        private readonly TelegramApiClient _api;
-
-        public void StartUpdatingThread(UpdateConfig config, Action<Update[]> onUpdate)
+        public async Task StartUpdatingThreadAsync(UpdateConfig config, Action<Update[]> onUpdate)
         {
-            var pongReqNeed = true;
-            _stop = false;
-            _thr = new Thread(async () =>
+            var pingReqNeed = true;
+            
+            _stopped = false;
+
+            while (!_stopped)
             {
-                while (!_stop)
-                {
-                    config.Offset = _lastId + 1;
-                    var response =
-                        await _api.RequestAsync<Update[]>("getUpdates", HttpMethod.Get, config.ToJson());
+	            config.Offset = _lastId + 1;
+	            
+	            var response =
+		            await _api.RequestAsync<Update[]>("getUpdates", HttpMethod.Get, config.ToJson());
 
-                    if (response != null)
-                    {
-                        if (pongReqNeed)
-                        {
-                            Logger.Log($"First update response got. Count: {response.Length}.",
-                                LogSource.TelegramApiServer);
-                            pongReqNeed = false;
-                        }
+	            if (response != null)
+	            {
+		            if (pingReqNeed)
+		            {
+			            Logger.Log($"First update response got. Count: {response.Length}.",
+				            LogSource.TelegramApiServer);
+			            
+			            pingReqNeed = false;
+		            }
 
-                        if (response.Length > 0)
-                        {
-	                        _lastId = response.Select(x => x.UpdateId).OrderByDescending(x => x).First();
-                        }
+		            if (response.Length > 0)
+		            {
+			            _lastId = response.Select(x => x.UpdateId).OrderByDescending(x => x).First();
+		            }
 
-                        onUpdate?.Invoke(response);
-                    }
-                    else
-                    {
-                        Logger.Log("Telegram API server is unavailable now.", LogSource.Warn);
-                        pongReqNeed = false;
-                    }
+		            onUpdate?.Invoke(response);
+	            }
+	            else
+	            {
+		            Logger.Log("Telegram API server is unavailable now.", LogSource.Warn);
+		            
+		            pingReqNeed = false;
+	            }
 
-                    Thread.Sleep(TimeSpan.FromSeconds(1));
-                }
-            });
-
-            _thr.Start();
+	            await Task.Delay(OneSecond);
+            }
         }
 
         public void StopUpdatingThread()
         {
-            _stop = true;
+            _stopped = true;
         }
     }
 }
